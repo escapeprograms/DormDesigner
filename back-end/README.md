222d766bea414615d2649cd79d4897378213c632
akoduru26 <136635166+akoduru26@users.noreply.github.com>
2024-10-24 13:48:53 -0400

    cleanup

--- back-end/README.md
+++ back-end/README.md
@@ -2,8 +2,4 @@
 
 This is some information about connecting to MongoDB Atlas, our database solution. 
 To run the system while connected to the database:
-- Run 'npm install mongodb'
-The following piece of code in 'index.js' connects the project to MongoDB.
-
- ```await mongoose.connect(process.env.MONGO_URL);
-      console.log("Connected to DB");```
+- Run 'npm install'
--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,8 @@
 
-//'mongodb+srv://ananyakoduru22:<Sj3Qgq4JcLUnPAgH>@cluster0.uvjkd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-import { MongoClient, ServerApiVersion } from 'mongodb';  // Use import for MongoDB
+import { MongoClient, ServerApiVersion } from 'mongodb';  
 
-const uri = 'mongodb+srv://ananyakoduru22:wJJdnMfiXTaPEyFs@cluster0.pjmeg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; // Ensure you're loading the URI from environment variables
+const uri = 'mongodb+srv://ananyakoduru22:wJJdnMfiXTaPEyFs@cluster0.pjmeg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; 
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -15,13 +13,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
     await client.close();
   }
 }
