98381aaae707fb2f9d7ced82d8aab4fc15854577
akoduru26 <136635166+akoduru26@users.noreply.github.com>
2024-10-24 13:46:45 -0400

    Finished set up for mongodb
    - added type: module to package-json
    - added nodemon to make npm . the backend run script
    - mongourl is in index.js

--- back-end/README.md
+++ back-end/README.md
@@ -5,5 +5,5 @@ To run the system while connected to the database:
 - Run 'npm install mongodb'
 The following piece of code in 'index.js' connects the project to MongoDB.
 
- await mongoose.connect(process.env.MONGO_URL);
-      console.log("Connected to DB");
+ ```await mongoose.connect(process.env.MONGO_URL);
+      console.log("Connected to DB");```
--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,29 @@
-// import dotenv from 'dotenv';
-// dotenv.config();
-// import mongoose from "mongoose";
 
+//'mongodb+srv://ananyakoduru22:<Sj3Qgq4JcLUnPAgH>@cluster0.uvjkd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+import { MongoClient, ServerApiVersion } from 'mongodb';  // Use import for MongoDB
 
-// await mongoose.connect(process.env.MONGO_URL);
-//     console.log("Connected to DB");
-console.log("hee");
+const uri = 'mongodb+srv://ananyakoduru22:wJJdnMfiXTaPEyFs@cluster0.pjmeg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; // Ensure you're loading the URI from environment variables
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
