62594de18f2ee2424e1ac47c2e7365d0e91efaf7
alekach <ajlekachman@icloud.com>
2024-10-27 10:50:16 -0700

    Adding Login Page
    needs react-router-dom

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Editor from './Components/Editor/Editor';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import LoginPage from './Components/LoginPage/LoginPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route exact path ="/" element = {<LoginPage/>}></Route>
+        <Route exact path = "/editor" element = {<Editor/>}></Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
