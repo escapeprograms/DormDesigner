e31791485b32bfb80983ac8cd785b3b8bccdea73
Archimedes Li <escapereovery@gmail.com>
2024-10-27 18:36:18 -0400

    Orbit Controls

--- dorm-designer/src/components/editor.js
+++ dorm-designer/src/components/editor.js
@@ -1,16 +1,11 @@
 import React, { useRef, useEffect } from 'react';
 import * as THREE from 'three';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
 
 import getFloorMesh from '../three-objects/floor';
 
 const Editor = () => {
     const mountRef = useRef(null);
-    //rotation settings
-    let targetRotation = 0;
-    let targetRotationOnPointerDown = 0;
-
-    let pointerX = 0;
-    let pointerXOnPointerDown = 0;
 
     useEffect(() => {
         const scene = new THREE.Scene();
@@ -20,11 +15,12 @@ const Editor = () => {
 
         renderer.setSize(window.innerWidth, window.innerHeight);
         mountRef.current.appendChild(renderer.domElement);
-        renderer.domElement.addEventListener( 'pointerdown', onPointerDown );
+
+        const controls = new OrbitControls(camera, renderer.domElement);
 
 
         //floor
-        const vertices = [
+        let vertices = [
             new THREE.Vector2(-3,-2.5),
             new THREE.Vector2(0,-2.5),
             new THREE.Vector2(0,-1.5),
@@ -47,7 +43,10 @@ const Editor = () => {
             new THREE.Vector2(-3,-2),
             new THREE.Vector2(-3,-3)
         ];
-        
+        vertices = vertices.map(v => {
+            return v.multiplyScalar(2)
+        })
+
         const material2 = new THREE.MeshBasicMaterial({ color: 0xffff00 });
         const floor = getFloorMesh(scene, vertices, 0xffff00);
 
@@ -58,47 +57,16 @@ const Editor = () => {
         const animate = () => {
             requestAnimationFrame(animate);
         
-            group.rotation.y = targetRotation;
+            controls.update();
             renderer.render(scene, camera);
         };
 
         animate();
 
-        //rotate via mouse controls
-        
-        function onPointerDown( event ) {
-
-            if ( event.isPrimary === false ) return;
-
-            pointerXOnPointerDown = event.clientX - window.innerWidth/2;
-            targetRotationOnPointerDown = targetRotation;
-
-            document.addEventListener( 'pointermove', onPointerMove );
-            document.addEventListener( 'pointerup', onPointerUp );
-
-        }
-
-        function onPointerMove( event ) {
-
-        if ( event.isPrimary === false ) return;
-
-        pointerX = event.clientX - window.innerWidth/2;
-
-        targetRotation = targetRotationOnPointerDown + ( pointerX - pointerXOnPointerDown ) * 0.02;
-
-        }
-
-        function onPointerUp() {
-
-        //if ( event.isPrimary === false ) return;
-
-        document.removeEventListener( 'pointermove', onPointerMove );
-        document.removeEventListener( 'pointerup', onPointerUp );
-
-        }
+       
 
         return () => {
-        mountRef.current.removeChild(renderer.domElement);
+            mountRef.current.removeChild(renderer.domElement);
         };
     }, []);
 
