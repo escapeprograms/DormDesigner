4581b4e0d8e74bc1ad9870e40dd6bb95e059e75a
Ananya Koduru <136635166+ak-dot-78@users.noreply.github.com>
2024-10-29 02:06:30 +0000

    Updated backend with express

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,9 @@
-
-
+import express from 'express';
 import { MongoClient, ServerApiVersion } from 'mongodb';  
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// database connection -------
 
 const uri = 'mongodb+srv://ananyakoduru22:wJJdnMfiXTaPEyFs@cluster0.pjmeg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; 
 
@@ -24,4 +27,29 @@ async function run() {
 
 run().catch(console.dir);
 
+// ----------------------------------------------------
+// setting up Express server to handle API requests from the frontend ----------------------------
+const app = express();
+const PORT = 5000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../dorm-designer/build')));
+
+
+app.get('/api/data', (req, res) => {
+  res.json({ message: 'Hello from backend!' });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dorm-designer/build', 'index.html'));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+// -----------------------------------------------
 
