485ad359c3b625b27d7e63f78aba647b4c71ba34
Ananya Koduru <136635166+ak-dot-78@users.noreply.github.com>
2024-11-13 03:50:48 +0000

    completed clerk integration

--- dorm-designer/src/App.js
+++ dorm-designer/src/App.js
@@ -1,18 +1,28 @@
 import './App.css';
 
 import Editor from './Components/Editor/Editor';
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from './Components/LoginPage/LoginPage';
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
 function App() {
   return (
     <Router>
+      {/* <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header> */}
       <Routes>
-        <Route exact path ="/" element = {<LoginPage/>}></Route>
-        <Route exact path = "/editor" element = {<Editor/>}></Route>
+        <Route exact path="/" element={<LoginPage />} />
+        <Route exact path="/editor" element={<Editor />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
