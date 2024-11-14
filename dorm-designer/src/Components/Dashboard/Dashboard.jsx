68855b8a709280236b14252fa0edc53c620821b5
alekach <ajlekachman@icloud.com>
2024-11-13 20:44:42 -0800

    Dashboard changes
    Routing to room selection
    Adding delete button

--- dorm-designer/src/Components/Dashboard/Dashboard.jsx
+++ dorm-designer/src/Components/Dashboard/Dashboard.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Dashboard.css'
 import { Link } from 'react-router-dom'
 
 const Dashboard = () => {
+
+  const [designs, setDesigns] = useState([]);
+
+  const addDesign = () => {
+    setDesigns([...designs, <div key = {designs.length} className = "design">New Design{designs.length+1}</div>]);
+  };
+
   return (
     <div>
       <nav className = "navbar">
         <h1>Your Designs</h1>
         <div className = "buttons">
-          <div className = "button"><Link to = "/editor">New Design</Link></div>
+          <div className = "button" onClick ={addDesign}><Link to = "/residential-area">New Design</Link></div>
+          <div className = "button">Delete Design</div>
           <div className = "button">Help</div>
           <div className = "button"><Link to = "/">Sign Out</Link></div>
         </div>
       </nav>
+      <div className = "designContainer">
+        {designs}
+      </div>
     </div>
   )
 }
